7bea7c421a2eaf64cb20e8348214d9b8600f64e6
UMC <umc1997@naver.com>
2020-12-09 01:35:48 +0900

    2020/12/09 Mode of Operations

--- main.py
+++ main.py
@@ -1,47 +1,126 @@
-'''
-1. convert : convert to byte
-2. padding : compute byte length and padding
-3. select cipher: AES DES SEED ~~
-4. select operation mode : ~~
-5. key_gen
-(6.) IV
-7. encryption
-8. decryption
-'''
 from Crypto.Cipher import AES
-from PIL import Image
-import binascii, os, random, struct
-    
-def convert2RGB(data):
-    r,g,b = tuple(map(lambda d: [data[i] for i in range(0,len(data)) \
-                                 if i % 3 == d], [0, 1, 2]))
-    pixels = tuple(zip(r,g,b))
-    return pixels
+from Crypto.Cipher import DES3
+import binascii
 
-def encrypt_bmp_file(key, mode, in_filename, out_filename = None):
+block_size = 16
 
-    im = Image.open(in_filename)
-    data = im.convert("RGB").tobytes()
-    original = len(data)
+def xor(a, b):
+    return bytes(x ^ y for x, y in zip(a, b))
 
-    pad_len = 16 - len(data) % 16
-    pad = pad_len.to_bytes(1, byteorder='big', signed=False) * pad_len
-    data += pad
+def encryptBlock(cipher, key, block):
+    if cipher == "AES":
+        encryptor = AES.new(key, AES.MODE_ECB)
+    elif cipher == "DES3":
+        encryptor = DES3.new(key, DES3.MODE_ECB)
+    return encryptor.encrypt(block)
 
-    encryptor = AES.new(key, mode)
-    encrypted = convert2RGB((encryptor.encrypt(data)[: original]))
+def decryptBlock(cipher, key, block):
+    if cipher == "AES":
+        decryptor = AES.new(key, AES.MODE_ECB)
+    elif cipher == "DES3":
+        decryptor = DES3.new(key, DES3.MODE_ECB)
+    return decryptor.decrypt(block)
 
-    im2 = Image.new(im.mode, im.size)
-    im2.putdata(encrypted)
+def encryptBytes(cipher, mode, key, iv, bytes):
+    # encoding (One-Zero padding)
+    padLen = (block_size - len(bytes) % block_size)
+    pad = bytes.fromhex("10")
+    pad += (bytes.fromhex("00") * (padLen - 1))
+    bytes += pad
 
-    im2.save(out_filename)
-    print("{} is encrypted".format(in_filename))
+    # encipher
+    encryptedBytes = bytearray(b'')
+    block_num = len(bytes) // block_size
+
+    for i in range(0, block_num):
+        currentBlock = bytes[i * block_size: (i + 1) * block_size]
+        if mode == "ECB":
+            encryptedBytes += encryptBlock(cipher, key, currentBlock)
+        elif mode == "CBC":
+            nextiv = encryptBlock(cipher, key, xor(currentBlock,iv))
+            encryptedBytes += nextiv
+            iv = nextiv
+        elif mode == "OFB":
+            nextiv = encryptBlock(cipher, key, iv)
+            encryptedBytes += xor(currentBlock, nextiv)
+            iv = nextiv
+        elif mode == "CFB":
+            nextiv = xor(encryptBlock(cipher, key, iv), currentBlock)
+            encryptedBytes += nextiv
+            iv = nextiv
+        elif mode == "CTR":
+            encryptedBytes += xor(encryptBlock(cipher, key, iv), currentBlock)
+            iv = xor(iv, (1).to_bytes(block_size, byteorder="big", signed=True))
+
+    return encryptedBytes
+
+def decryptBytes(cipher, mode, key, iv, bytes):
+    # decipher
+    decryptedBytes = bytearray(b'')
+    block_num = len(bytes) // block_size
+
+    for i in range(0, block_num):
+        currentBlock = bytes[i * block_size : (i + 1) * block_size]
+        if mode == "ECB":
+            decryptedBytes += decryptBlock(cipher, key, currentBlock)
+        elif mode == "CBC":
+            decryptedBytes += xor(decryptBlock(cipher, key, currentBlock), iv)
+            iv = currentBlock
+        elif mode == "OFB":
+            nextiv = encryptBlock(cipher, key, iv)
+            decryptedBytes += xor(currentBlock, nextiv)
+            iv = nextiv
+        elif mode == "CFB":
+            nextiv = xor(encryptBlock(cipher, key, iv), currentBlock)
+            decryptedBytes += nextiv
+            iv = currentBlock
+        elif mode == "CTR":
+            decryptedBytes += xor(encryptBlock(cipher, key, iv), currentBlock)
+            iv = xor(iv, (1).to_bytes(block_size, byteorder="big", signed=True))
+    # decoding
+    lastBlock = decryptedBytes[(block_num - 1) * block_size:]
+    lastBlockReverse = lastBlock[::-1]
+    for i in lastBlockReverse:
+        if i != 0:
+            decryptedBytes = decryptedBytes[:(block_num - 1) * block_size + block_size - i]
+            break
+
+    return decryptedBytes
+
+msg = bytes.fromhex(" 6bc1bee22e409f96e93d7e117393172a  \
+                     ae2d8a571e03ac9c9eb76fac45af8e51   \
+                     30c81c46a35ce411e5fbc1191a0a52ef   \
+                     f69f2445df4f9b17ad2b417be66c3710")
 
 
-key = bytes.fromhex("000102030405060708090a0b0c0d0e0f")
+cipher = "AES"
+#cipher = "DES3"
 
-mode = AES.MODE_ECB
-str1 = "sample_07.bmp"
-str2 = str1 + ".ecb.bmp"
-str3 = str2 + ".bmp"
-encrypt_bmp_file(key, mode, str1, str2)
+mode1 = "ECB"
+mode2 = "CBC"
+mode3 = "OFB"
+mode4 = "CFB"
+mode5 = "CTR"
+key = bytes.fromhex("2b7e151628aed2a6abf7158809cf4f3c")
+IV = bytes.fromhex("000102030405060708090a0b0c0d0e0f")
+en1 = encryptBytes(cipher, mode1, key, IV, msg)
+de1 = decryptBytes(cipher, mode1, key, IV, en1)
+en2 = encryptBytes(cipher, mode2, key, IV, msg)
+de2 = decryptBytes(cipher, mode2, key, IV, en2)
+en3 = encryptBytes(cipher, mode3, key, IV, msg)
+de3 = decryptBytes(cipher, mode3, key, IV, en3)
+en4 = encryptBytes(cipher, mode4, key, IV, msg)
+de4 = decryptBytes(cipher, mode4, key, IV, en4)
+en5 = encryptBytes(cipher, mode5, key, IV, msg)
+de5 = decryptBytes(cipher, mode5, key, IV, en5)
+print(binascii.hexlify(msg))
+print(binascii.hexlify(en1))
+print(binascii.hexlify(de1))
+print(binascii.hexlify(en2))
+print(binascii.hexlify(de2))
+print(binascii.hexlify(en3))
+print(binascii.hexlify(de3))
+print(binascii.hexlify(en4))
+print(binascii.hexlify(de4))
+print(binascii.hexlify(en5))
+print(binascii.hexlify(de5))
